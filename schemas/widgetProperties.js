e881d3330ef309c4557b813edc1d47c8fe16111d
Andrzej Marciniak <katiamar63@gmail.com>
2019-11-20 10:09:42 -0500

    Added widgets definition example.

---
+++ schemas/widgetProperties.js
@@ -0,0 +1,123 @@
+const widgets = {
+    "pickerTool": {
+        "controlVisible": true,
+        "controlEnabled": true
+    },
+    "pinTool": {
+        "controlVisible": true,
+        "controlEnabled": true,
+        "properties": {
+            "markerSize": {
+                "controlVisible": true,
+                "controlEnabled": true,
+                "defaultValue": 2
+            },
+            "fontSize":{
+                "controlVisible": true,
+                "controlEnabled": true,
+                "defaultValue": 12
+            },
+            "clearAll":{
+                "controlVisible": true,
+                "controlEnabled": true
+            }
+        }
+    },
+    "projectionTool": {
+        "controlVisible": true,
+        "controlEnabled": true,
+        "properties": {
+            "range": {
+                "controlVisible": true,
+                "controlEnabled": true,
+                "defaultValue": 1
+            }
+        }
+    },
+    "linkAllTool": {
+        "controlVisible": true,
+        "controlEnabled": true
+    },
+    "crossHairTool": {
+        "controlVisible": true,
+        "controlEnabled": true
+    },
+    "brushTool": {
+        "controlVisible": true,
+        "controlEnabled": true,
+        "properties": {
+            "size": {
+                "controlVisible": true,
+                "controlEnabled": true,
+                "defaultValue": 1
+            },
+            "labelmap": {
+                "controlVisible": false,
+                "controlEnabled": false,
+                "defaultValue": 1,
+                "properties": {
+                    "options": {
+                        "label": "Medulla Oblongata",
+                        "iri": "",
+                        "color": "#f00",
+                        "value": 1
+                    }
+                }
+            },
+            "threshold": {
+                "controlVisible": false,
+                "controlEnabled": false,
+                "properties": {
+                    "lowerValueByDefault": 0,
+                    "upperValueByDefault": 65000,
+                    "probeControlVisible": true,
+                    "probeControlEnabled": true
+                }
+            }
+        }
+    },
+    "fillingTool": {
+        "controlVisible": true,
+        "controlEnabled": true,
+        "properties": {
+            "labelmap": {
+                "controlVisible": false,
+                "controlEnabled": false,
+                "defaultValue": 1,
+                "properties": {
+                    "options": [{
+                        "label": "Lesion",
+                        "iri": "",
+                        "color": "#f00",
+                        "value": 1
+                    }]
+                }
+            }
+        }
+    },
+    "eraserTool": {
+        "controlVisible": true,
+        "controlEnabled": true,
+        "properties": {
+            "size": {
+                "controlVisible": true,
+                "controlEnabled": true,
+                "defaultValue": 1
+            }
+        }
+    },
+    "labelmapUndoRedoTool": {
+        "controlVisible": true,
+        "controlEnabled": true
+    },
+    "labelmapOpacity": {
+        "controlVisible": true,
+        "controlEnabled": true,
+        "defaultValue": 1
+    },
+    "annotationOpacity": {
+        "controlVisible": true,
+        "controlEnabled": true,
+        "defaultValue": 1
+    }
+};
